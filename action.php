ed35efcb81d750ae25ae832aeffeb9789fa780db
Yi Ping <7th.cubic.sg@gmail.com>
2020-06-26 12:30:03 +0800

    Splitting Prerelease

--- action.php
+++ action.php
@@ -37,6 +37,17 @@ function debug(string $message) : void {
 }
 
 /**
+ * Check if a version is prerelease semantic or not
+ *
+ * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+ * @param string $version
+ * @return bool
+ */
+function isSemanticPreReleaseVersion(string $version) : bool {
+    return 0 < preg_match('/^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/', $version);
+}
+
+/**
  * Check if a version is semantic or not
  *
  * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
@@ -44,14 +55,48 @@ function debug(string $message) : void {
  * @return bool
  */
 function isSemanticVersion(string $version) : bool {
-    return 0 < preg_match('/^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/', $version);
+	return 0 < preg_match('/^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/', $version);
 }
 
+function removeVersions(string $packageName, array $versions, int $keepCount, array &$removedPackages) {
+	global $repoNameWithOwner, $versionsLimit, $deletePackageVersion, $clientId, $client;
+	$numVersions = min($versionsLimit, count($versions));
+	
+	 for ($i = $keepCount; $i < $numVersions; $i++) {
+		$packageVersionId       = $versions[$i]['id'];
+        $packageVersion         = $versions[$i]['version'];
+        $packageNameWithVersion = sprintf('%s:%s', $packageName, $packageVersion);
+		if ('docker-base-layer' === $packageVersion) {
+            // Removing this specific version of a Docker package triggers a bug in GitHub
+            // Packages. Keep this safeguard until the bug has been resolved.
+            continue;
+        }  
+		try {
+            $client->post('graphql', [
+                'headers' => [
+                    'Accept' => 'application/vnd.github.package-deletes-preview+json', // Header required for the deletePackageVersion mutation to be available
+                ],
+                'json' => [
+                    'query' => sprintf($deletePackageVersion, $clientId, $packageVersionId)
+                ],
+            ]);
+        } catch (ClientException $e) {
+            fail(sprintf('[%s] [%s] Remove package version failed: %s', $repoNameWithOwner, $packageNameWithVersion, $e->getResponse()->getBody()->getContents()));
+        }
+		
+        debug(sprintf('[%s] [%s] Remove package version', $repoNameWithOwner, $packageNameWithVersion));
+
+        $removedPackages[] = $packageNameWithVersion;
+	 }
+}
+
+$keepPrereleaseVersion = (int) getenv('INPUT_KEEP_PRERELEASE_VERSIONS') ?: 5;
+$removePrereleaseSemver  = 'true' === getenv('INPUT_REMOVE_PRERELEASE_SEMVER');
 $token             = (string) getenv('GITHUB_TOKEN');
 $keepVersions      = (int) getenv('INPUT_KEEP_VERSIONS') ?: 5;
 $removeSemver      = 'true' === getenv('INPUT_REMOVE_SEMVER');
 $repoNameWithOwner = (string) getenv('GITHUB_REPOSITORY');
-$clientId          = 'navikt/remove-package-versions';
+$clientId          = 'epitomeglobal/remove-package-versions';
 
 if (empty($token)) {
     fail('Missing GITHUB_TOKEN');
@@ -132,46 +177,40 @@ foreach ($packageNodes as $packageNode) {
     $packageName = $packageNode['name'];
 
     $versionNodes = $packageNode['versions']['nodes'];
-    $numVersions = min($versionsLimit, $packageNode['versions']['totalCount']);
+	$numVersions = 0;
+	
+	if($removeSemver || $removePrereleaseSemver) {		
+		$prVersions = array();
+		$versions = array();
+			
+		foreach($versionNodes as $versionNode) {
+			if(isSemanticVersion($versionNode['version'])) {
+				$versions[] = $versionNode;
+			}
+			else if(isSemanticPreReleaseVersion($versionNode['version'])) {
+				$prVersions[] = $versionNode;
+			}
+		}
+		if($versions <= $keepVersions) {
+			debug(sprintf('[%s] [%s] Package has fewer than %d versions, no need for removal', $repoNameWithOwner, $packageName, $keepVersions));		
+		} else {
+			removeVersions($packageName, $versions, $keepVersions, $removedPackages);	
+		}
+		if($prVersions <= $keepPrereleaseVersion) {
+			debug(sprintf('[%s] [%s] Package has fewer than prerelease %d versions, no need for removal', $repoNameWithOwner, $packageName, $keepPrereleaseVersion));
+		} else {
+			removeVersions($packageName, $prVersions, $keepPrereleaseVersion, $removedPackages);
+		}
+	} else {
+		$numVersions = min($versionsLimit, $packageNode['versions']['totalCount']);
 
-    if ($numVersions <= $keepVersions) {
-        debug(sprintf('[%s] [%s] Package has fewer than %d versions, no need for removal', $repoNameWithOwner, $packageName, $keepVersions));
-        continue;
-    }
-
-    for ($i = $keepVersions; $i < $numVersions; $i++) {
-        $packageVersionId       = $versionNodes[$i]['id'];
-        $packageVersion         = $versionNodes[$i]['version'];
-        $packageNameWithVersion = sprintf('%s:%s', $packageName, $packageVersion);
-
-        if ('docker-base-layer' === $packageVersion) {
-            // Removing this specific version of a Docker package triggers a bug in GitHub
-            // Packages. Keep this safeguard until the bug has been resolved.
-            continue;
-        }
-
-        if (!$removeSemver && isSemanticVersion($packageVersion)) {
-            debug(sprintf('[%s] [%s] Semantic versions will not be removed unless remove-semver is set to true', $repoNameWithOwner, $packageNameWithVersion));
-            continue;
-        }
-
-        debug(sprintf('[%s] [%s] Remove package version', $repoNameWithOwner, $packageNameWithVersion));
-
-        try {
-            $client->post('graphql', [
-                'headers' => [
-                    'Accept' => 'application/vnd.github.package-deletes-preview+json', // Header required for the deletePackageVersion mutation to be available
-                ],
-                'json' => [
-                    'query' => sprintf($deletePackageVersion, $clientId, $packageVersionId)
-                ],
-            ]);
-        } catch (ClientException $e) {
-            fail(sprintf('[%s] [%s] Remove package version failed: %s', $repoNameWithOwner, $packageNameWithVersion, $e->getResponse()->getBody()->getContents()));
-        }
-
-        $removedPackages[] = $packageNameWithVersion;
-    }
+		if ($numVersions <= $keepVersions) {
+			debug(sprintf('[%s] [%s] Package has fewer than %d versions, no need for removal', $repoNameWithOwner, $packageName, $keepVersions));
+			continue;
+		}
+			
+		removeVersions($packageName, $versionNodes, $keepVersions, $removedPackages);	
+	}
 }
 
 echo sprintf('::set-output name=removed-package-versions::%s', json_encode(array_map(function(string $version) use ($repoNameWithOwner) : string {
